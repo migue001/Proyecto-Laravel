285136e0d9fd95e5e86c7c1f5a7a5747f143c342
Miguel Rangel <40450007+migue001@users.noreply.github.com>
2024-08-22 02:53:31 -0600

    Vistas - Componentes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,6 @@ class HomeController extends Controller
 {
     public function index(){
         
-        return "Welcome to the Home Page";
+        return view('home');
     }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,12 +7,15 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(){
-        return "Aqui se mostraran todos los posts"; 
+        return view('Posts.index');
     }
     public function create(){
-        return "Se mostrara un formulario para crear";
+        return view('Posts.create');
     }
     public function show($post){
-        return "Se mostrara el post {$post}";
+        //compact('post); ['post' => $post]
+        return view('Posts.show', [
+            'post' => $post
+        ]);
     }
 }
